9aa4c76c1a0c9d78c82f77f49b3af9608ba15b8f
yasinbashar <107603927+yasinbashar@users.noreply.github.com>
2024-04-26 16:42:17 +0600

    updated all authentication, crud operation

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const {
-    getContacts,
-    CreateContact,
-    getContact,
-    updateContact,
-    deleteContact,
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
 } = require("../controllers/contactController");
-router.route("/").get(getContacts);
+const validateToken = require("../middleware/validateTokenHandler");
 
-router.route("/").post(CreateContact);
-
-router.route("/:id").get(getContact);
-
-router.route("/:id").put(updateContact);
-
-router.route("/:id").delete(deleteContact);
-
+router.use(validateToken);
+router.route("/").get(getContacts).post(createContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./Config/dpConnection");
+
+connectDb();
+
 const app = express();
 
 
 const port = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes")); 
+app.use(errorHandler);
+
 
 app.listen(port, () =>{
     console.log(`Server runnimg on port ${port}`);
